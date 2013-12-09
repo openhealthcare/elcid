0b7ead12889b0b8321d09a3b65df308529207cc9
David Miller <david@deadpansincerity.com>
2013-12-09 20:42:11 +0000

    Tidies discharge flow. refs #8

--- elcid/assets/js/elcid/controllers.js
+++ elcid/assets/js/elcid/controllers.js
@@ -1,52 +1,57 @@
+//
+// This is the eLCID custom implementation of a discharge episode controller.
+// It is included from the eLCID extra aplication template as defined in
+// settings.py
+//
 controllers.controller('DischargeEpisodeCtrl', function($scope, $timeout,
                                                         dialog, episode, currentTag) {
-	$timeout(function() {
-		dialog.modalEl.find('input,textarea').first().focus();
-	});
+    $timeout(function() {
+	dialog.modalEl.find('input,textarea').first().focus();
+    });
 
-	var currentCategory = episode.location[0].category;
-	var newCategory;
+    $scope.currentCategory = episode.location[0].category;
+    var newCategory;
 
-	if (currentCategory == 'Inpatient') {
-		newCategory = 'Discharged';
-	} else if (currentCategory == 'Review' || currentCategory == 'Followup') {
-		newCategory = 'Unfollow';
-	} else {
-		newCategory = currentCategory;
-	}
+    if ($scope.currentCategory == 'Inpatient') {
+	newCategory = 'Discharged';
+    } else if ($scope.currentCategory == 'Review' || $scope.currentCategory == 'Followup') {
+	newCategory = 'Unfollow';
+    } else {
+	newCategory = $scope.currentCategory;
+    }
 
-	$scope.editing = {
-		category: newCategory,
-		//date: new Date()
-	};
+    $scope.editing = {
+	category: newCategory,
+	//date: new Date()
+    };
 
     $scope.episode = episode.makeCopy();
     $scope.episode.discharge_date = moment().format('DD/MM/YYYY');
 
-	$scope.discharge = function() {
-		var location = episode.getItem('location', 0);
-		var attrs = location.makeCopy();
+    $scope.discharge = function() {
+	var location = episode.getItem('location', 0);
+	var attrs = location.makeCopy();
 
-		if ($scope.editing.category != 'Unfollow') {
-			attrs.category = $scope.editing.category;
-		}
+	if ($scope.editing.category != 'Unfollow') {
+	    attrs.category = $scope.editing.category;
+	}
 
-		if ($scope.editing.category != 'Followup') {
-			attrs.tags[currentTag] = false;
-		}
+	if ($scope.editing.category != 'Followup') {
+	    attrs.tags[currentTag] = false;
+	}
 
-		location.save(attrs).then(function() {
+	location.save(attrs).then(function() {
             // TODO: Don't hard code workflow in OPAL.
             if($scope.editing.category!= 'Followup'){
                 $scope.episode.active = false;
             }
             episode.save($scope.episode).then(function(){
-			    dialog.close('discharged');
+		dialog.close('discharged');
             });
-		});
-	};
+	});
+    };
 
-	$scope.cancel = function() {
-		dialog.close('cancel');
-	};
+    $scope.cancel = function() {
+	dialog.close('cancel');
+    };
 });
