49516d8b2568ddc76d534e5840c3fa013492677f
David Miller <david@deadpansincerity.com>
2013-12-10 11:51:54 +0000

    Sendgridify for Heroku mails.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn elcid.wsgi
+web: cp elcid/local_settings_heroku.py elcid/local_settings.py && egunicorn elcid.wsgi
---
+++ elcid/local_settings_heroku.py
@@ -0,0 +1,2 @@
+from sendgrifiy import sendgridify
+EMAIL_HOST, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD, EMAIL_PORT, EMAIL_USE_TLS = sendgridify()
