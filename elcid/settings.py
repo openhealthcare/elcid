d866069431fc8bf4835cc63fd22dfa16bb1e9e56
David Miller <david@deadpansincerity.com>
2013-12-12 13:26:35 +0000

    Tinkering with contact email settings.

--- elcid/settings.py
+++ elcid/settings.py
@@ -211,7 +211,7 @@ OPAL_TAGS_MODULE = 'elcid.tags'
 OPAL_BRAND_NAME = 'eLCID'
 OPAL_EXTRA_APPLICATION = 'elcid/extra_application.html'
 
-CONTACT_EMAIL = 'michael.marks@uclh.nhs.uk,david@openhealthcare.org.uk'
+CONTACT_EMAIL = ['michael.marks@uclh.nhs.uk', 'david@openhealthcare.org.uk']
 DEFAULT_FROM_EMAIL = 'elcid.feedback@openhealthcare.org.uk'
 
 EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
