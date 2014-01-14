cac98cff4e0414d9a92ac72a193b194c9fa1003b
David Miller <david@deadpansincerity.com>
2014-01-14 22:11:07 +0000

    Ignore googlebot crawling the github link for the error test page.
    closes #24

--- elcid/views.py
+++ elcid/views.py
@@ -7,6 +7,7 @@ import random
 from django import forms
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.http import HttpResponse
 from django.views.generic import TemplateView, FormView, View
 import letter
 from letter.contrib.contact import EmailForm, EmailView
@@ -53,6 +54,8 @@ class Error500View(View):
     Demonstrative 500 error to preview templates.
     """
     def get(self, *args, **kwargs):
+        if self.request.META['HTTP_USER_AGENT'].find('Googlebot') != -1:
+            return HttpResponse('No')
         raise Exception("This is a deliberate error")
 
 
