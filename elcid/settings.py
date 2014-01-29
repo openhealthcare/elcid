d247122f8364c5007beaac2e272449245cd87de3
David Miller <david@deadpansincerity.com>
2014-01-29 11:54:46 +0000

    Use the settings context processor in elCID.
    refs #68

--- elcid/settings.py
+++ elcid/settings.py
@@ -138,6 +138,7 @@ TEMPLATE_CONTEXT_PROCESSORS= (
     'django.core.context_processors.static',
     'django.core.context_processors.tz',
     'django.contrib.messages.context_processors.messages',
+    'opal.context_processors.settings',
 )
 
 INSTALLED_APPS = (
