6c5ccfe89a5f84fb344fc948389fc4f4d563b2de
David Miller <david@deadpansincerity.com>
2014-02-17 18:39:39 +0000

    Add reversionmiddleware

--- elcid/settings.py
+++ elcid/settings.py
@@ -114,6 +114,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'opal.middleware.DjangoReversionWorkaround',
     'reversion.middleware.RevisionMiddleware',
     'axes.middleware.FailedLoginMiddleware',
     # Uncomment the next line for simple clickjacking protection:
