8afb21bf1ddc09e5b25baa3161c8a1e5d9c63782
Andy Lulham <a.lulham@gmail.com>
2014-07-29 14:37:50 +0100

    Get travis to just use rake.
    How do you feel about this, Travis?
    
    refs openhealthcare/opal#217

--- config/karma.conf.travis.js
+++ config/karma.conf.travis.js
@@ -1,7 +1,7 @@
 module.exports =  function(config){
     config.set({
         frameworks: ['jasmine'],
-        browsers: ['Chrome'],
+        browsers: ['Firefox'],
         basePath:  '/home/travis/virtualenv/python2.7/src/opal/opal/static/js',
 
         files: [
