df3e4917bb8ba94c793ce2930f484f3d10ffecfa
David Miller <david@deadpansincerity.com>
2014-08-09 10:27:56 +0100

    Allow patients to be accepted or rejected from the OPAT service refs #193

--- elcid/flow.py
+++ elcid/flow.py
@@ -7,18 +7,18 @@ flows = {
         'enter': {
             'controller': 'HospitalNumberCtrl',
             'template'  : '/templates/modals/hospital_number.html/'
-            },
+        },
         'exit': {
             'controller': 'ElcidDischargeEpisodeCtrl',
             'template'  : '/templates/modals/discharge_episode.html/'
         }
-        # 'opat': {
-        #     'default': {
-        #         'exit': {
-        #             'controller': 'OPATDischargeCtrl',
-        #             'template'  : '/templates/modals/discharge_opat_episode.html/'
-        #         }
-        #     }
-        # }
+    },
+    'opat': {
+        'default': {
+            'exit': {
+                'controller': 'OPATDischargeCtrl',
+                'template'  : '/templates/modals/discharge_opat_episode.html/'
+            }
+        }
     }
 }
