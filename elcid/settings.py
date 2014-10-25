106b30f1b5246443c2cd9d16917cc5ea07abe238
David Miller <david@deadpansincerity.com>
2014-10-25 16:44:26 +0100

    Updates to lab test
    refs openhealthcare/opal-research#43
    refs openhealthcare/opal-research#49

--- elcid/settings.py
+++ elcid/settings.py
@@ -248,10 +248,10 @@ COVERAGE_EXCLUDE_MODULES = ('elcid.migrations', 'elcid.tests',
                             'opal.wsgi')
 
 # Research settings
-LIST_SCHEMA_RESEARCH_PRACTITIONER = 'elcid.schema.list_columns_research_practitioner'
-LIST_SCHEMA_SCIENTIST = 'elcid.schema.list_columns_scientist'
+LIST_SCHEMA_RESEARCH_PRACTITIONER = "elcid.schema.list_columns_research_practitioner"
+LIST_SCHEMA_SCIENTIST = "elcid.schema.list_columns_scientist"
 
-INTEGRATING = True
+INTEGRATING = False
 GLOSSOLALIA_URL = 'http://localhost:5000/'
 GLOSSOLALIA_NAME = 'elcid'
 
