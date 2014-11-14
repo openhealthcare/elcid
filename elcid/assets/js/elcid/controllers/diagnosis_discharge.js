df40dd04f5d9e868fc547331b938afd9aadc557d
David Miller <david@deadpansincerity.com>
2014-11-14 18:20:33 +0000

    Working discharge for final diagnoses.

--- elcid/assets/js/elcid/controllers/diagnosis_discharge.js
+++ elcid/assets/js/elcid/controllers/diagnosis_discharge.js
@@ -11,12 +11,28 @@ controllers.controller(
         Flow,
         tags, schema, options, episode){
 
+        $scope.tags = tags;
         $scope.episode = episode;
         $scope.editing = {
             primary_diagnosis  : null,
             secondary_diagnosis: [{condition: null, co_priamary: false, id: 1},
                                   {condition: null, co_priamary: false, id: 2}]
 
+        }
+
+        // 
+        // We should deal with the case where we're confirming discharge
+        //
+        if(episode.primary_diagnosis.length == 1){
+            $scope.editing.primary_diagnosis = episode.primary_diagnosis[0].condition;
+        };
+        if(episode.secondary_diagnosis && episode.secondary_diagnosis.length > 0){
+            $scope.editing.secondary_diagnosis = _.map(
+                episode.secondary_diagnosis,
+                function(d){
+                    return d.makeCopy();
+                }
+            );
         }
         
 	for (var name in options) {
@@ -26,10 +42,20 @@ controllers.controller(
 	};
         
         initialize = function(){
-            var classic_discharge = Flow(
-                'exit', schema, options, { episode: episode }
-            );
-            classic_discharge.then(
+
+            var classic_discharge = Flow.flow_for_verb('exit')
+            var classic_result    = $modal.open({
+		templateUrl: classic_discharge.template,
+		controller:  classic_discharge.controller,
+		resolve: {
+		    episode: function() { return $scope.episode; },
+                    tags   : function() { return $scope.tags; },
+                    options: function() { return options; },
+                    schema : function() { return schema; }
+		}
+	    }).result
+
+            classic_result.then(
                 function(result){ // Resolve
                     console.log('collect our extra data now !')
                     
@@ -44,10 +70,13 @@ controllers.controller(
         // Add an extra Secondary diagnosis option to the list
         // 
         $scope.addSecondary = function(){
-            var d = {condition: null, co_priamary: false, id: $scope.editing.secondary_diagnosis.length + 1}
+            var d = {
+                condition: null,
+                co_priamary: false,
+                id: $scope.editing.secondary_diagnosis.length + 1
+            };
             $scope.editing.secondary_diagnosis.push(d)
         };
-        
         
         // Let's have a nice way to kill the modal.
         $scope.cancel = function() {
@@ -58,7 +87,9 @@ controllers.controller(
             var primary = episode.newItem('primary_diagnosis');
             primary.save({condition: $scope.editing.primary_diagnosis}).then(
                 function(){
-                    var secondaries = _.map($scope.editing.secondary_diagnosis, function(sd){
+                    var secondaries = _.map(
+                        _.filter($scope.editing.secondary_diagnosis, function(sd){ return sd.condition!= null }), 
+                        function(sd){
                         var secondary = $scope.episode.newItem('secondary_diagnosis');
                         delete sd.id;
                         return secondary.save(sd)
@@ -67,6 +98,10 @@ controllers.controller(
                         $modalInstance.close('discharged');
                     });
                 });
+        };
+
+        $scope.invalid = function() {
+            return $scope.editing.primary_diagnosis == null
         };
         
         initialize();
