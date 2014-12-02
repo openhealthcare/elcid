15b7345a0499de32d4d723e7d0a70f53cb444691
David Miller <david@deadpansincerity.com>
2014-12-02 19:09:01 +0000

    Add the discharge date filter for our FDR round.
    refs #364

--- elcid/wardrounds.py
+++ elcid/wardrounds.py
@@ -2,8 +2,9 @@
 [Virtual] Ward Round implementations
 """
 from opal.models import Episode
-
 from wardround import WardRound
+
+from elcid.models import PrimaryDiagnosis
 
 class MicroHaem(WardRound):
     name        = 'Micro Haem'
@@ -17,7 +18,14 @@ class MicroHaem(WardRound):
 class FinalDiagnosisReview(WardRound):
     name        = 'Final Diagnosis Review'
     description = 'Discharged Patients with a final diagnosis for consultant review.'
-
+    
+    filter_template = 'wardrounds/final_diagnosis_filter.html'
+    filters    = {
+        'discharge_from': 'episode.discharge_date >= moment(value, "DD-MM-YYYY")',
+        'discharge_to'  : 'episode.discharge_date <= moment(value, "DD-MM-YYYY")'
+    }
+    
     @staticmethod
     def episodes():
-        return Episode.objects.filter(active=True)
+        unconfirmed = PrimaryDiagnosis.objects.filter(confirmed=False)
+        return set([d.episode for d in unconfirmed])
