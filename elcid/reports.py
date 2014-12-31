16cd65924f968070dc1e33c0b18bdfee4864b657
David Miller <david@deadpansincerity.com>
2014-12-31 19:12:06 +0000

    Strip report pending proper implementation
    refs #176

--- elcid/reports.py
+++ elcid/reports.py
@@ -15,20 +15,25 @@ def usage():
     Return data for a usage report.
     """
     episodes = collections.defaultdict(int)
-    for episode in Episode.objects.all():
-        start = episode.date_of_admission
-        end = episode.discharge_date
-        if not end:
-            end = dt.date.today()
-
-        while start <= end:
-            episodes[start] +=1
-            start += dt.timedelta(days=1)
+    # for episode in Episode.objects.all():
+    #     if not episode.date_of_admission:
+    #         continue
+    #     episodes[episode.date_of_admission] +=1
+        # start = episode.date_of_admission
+        # if not start:
+        #     continue
+        # end = episode.discharge_date
+        # if not end:
+        #     end = dt.date.today()
+        # while start <= end:
+            
+        #     start += dt.timedelta(days=1)
 
     return dict(
         num_users=User.objects.count(),
         num_episodes=Episode.objects.count(),
         num_diagnoses=Diagnosis.objects.count(),
         num_patients=Patient.objects.count(),
+#        episodes={}
         episodes=dict(**episodes)
         )
