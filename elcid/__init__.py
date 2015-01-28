68378e1ef265ee5b1dbaa57b6709e816b689b37e
David Miller <david@deadpansincerity.com>
2015-01-28 09:04:05 +0000

    Move extra application into OpalApplication

--- elcid/__init__.py
+++ elcid/__init__.py
@@ -8,3 +8,10 @@ from opal import application
 class Application(application.OpalApplication):
     schema_module = 'elcid.schema'
     flow_module   = 'elcid.flow'
+    javascripts   = [
+        'js/elcid/routes.js',
+        'js/elcid/controllers.js',
+        'js/elcid/controllers/diagnosis_hospital_number.js',
+        'js/elcid/controllers/diagnosis_add_episode.js',
+        'js/elcid/controllers/diagnosis_discharge.js'
+    ]
