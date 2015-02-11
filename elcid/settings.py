74ff5e7cd923610ce41a4afda4b1f6f49860f057
David Miller <david@deadpansincerity.com>
2015-02-11 15:40:58 +0000

    Updates brand name default

--- elcid/settings.py
+++ elcid/settings.py
@@ -218,7 +218,7 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 # New modular settings!
 # !!! TODO: Are these how we want to discover these ? 
 OPAL_OPTIONS_MODULE = 'elcid.options'
-OPAL_BRAND_NAME = 'elCID - OPAT'
+OPAL_BRAND_NAME = 'elCID - 0.4'
 OPAL_LOG_OUT_MINUTES = 15
 OPAL_LOG_OUT_DURATION = OPAL_LOG_OUT_MINUTES*60*1000
 
