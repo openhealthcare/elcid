7cb4b659f7dae3e43b250563293d3f2e22bef166
David Miller <david@deadpansincerity.com>
2015-02-16 10:10:04 +0000

    Add target category

--- elcid/referrals.py
+++ elcid/referrals.py
@@ -7,5 +7,6 @@ class HTDWalkInRoute(ReferralRoute):
     name = 'HTD Walk In Clinic'
     description = 'The HTD Walk-In clinic provides a primary care service to returning travelers.'
     target_teams = ['walkin', 'walkin_triage']
+    target_category = 'Walkin'
     success_link = '/#/list/walkin'
 
