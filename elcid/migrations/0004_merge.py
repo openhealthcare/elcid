68929a243945f2b6ec382ad5051a61274cebf652
fredkingham <fredkingham@gmail.com>
2015-10-12 15:27:41 +0100

    fixing migration clashes

--- elcid/migrations/0004_merge.py
+++ elcid/migrations/0004_merge.py
@@ -9,6 +9,7 @@ class Migration(migrations.Migration):
     dependencies = [
         ('elcid', '0003_auto_20151007_1100'),
         ('elcid', '0003_auto_20150917_1447'),
+        ('elcid', '0004_auto_20150918_1450'),
     ]
 
     operations = [
