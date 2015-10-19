e600945d99fc78b417f28d5ce3a933df9cc94004
fredkingham <fredkingham@gmail.com>
2015-10-19 11:04:22 +0100

    add migration for creating the consultant at discharge singletons

---
+++ elcid/migrations/0016_create_consultant_singletons.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+def create(apps, schema_editor):
+    Episode = apps.get_model("opal", "Episode")
+    ConsultantAtDischarge = apps.get_model("elcid", "ConsultantAtDischarge")
+    episodes = Episode.objects.all()
+    consultants = [ConsultantAtDischarge(episode=episode) for episode in episodes]
+    ConsultantAtDischarge.objects.bulk_create(consultants)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('elcid', '0015_antimicrobial_no_antimicriobials'),
+    ]
+
+    operations = [
+        migrations.RunPython(create)
+    ]
