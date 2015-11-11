53748a2d4d74cd798c4ebf071ef3688d1080984d
fredkingham <fredkingham@gmail.com>
2015-11-11 14:37:30 +0000

    removing symptom 1 2 3

--- data/lookuplists/lookuplist_syncher.py
+++ data/lookuplists/lookuplist_syncher.py
@@ -1,6 +1,7 @@
 import csv
 import json
 import sys
+import pprint
 
 # a simple lookup list loader
 # takes in a csv, with the headers name, synonym 1, synonym 2, synonym 3
@@ -8,81 +9,163 @@ import sys
 # takes in the lookuplists.json file
 # syncs the 2
 
-
-class Loader(object):
-
+class Differ(object):
     def __init__(self, lookuplist_type, to_load_csv, lookuplist_file="lookuplists.json"):
         self.lookuplist_type = lookuplist_type
         self.to_load_csv = to_load_csv
         self.lookuplist_file = lookuplist_file
 
-    # we assume words that are abbreviated are already uppercase
-    def case_it(self, word):
-        if word.isupper():
-            return word
-        else:
-            return word.title()
-
-    def existing_terms_and_synonyms(self, existing_json):
-        all_synonyms = set()
+    def names_to_synonyms(self, some_json):
         names_to_synonyms = {}
 
-        for row in existing_json:
-            name = self.case_it(row["name"])
-            names_to_synonyms[name] = set(row["synonyms"])
-            all_synonyms = all_synonyms.union(self.case_it(i) for i in row["synonyms"])
+        for row in some_json:
+            name = row["name"].lower()
+            names_to_synonyms[name] = set(i.lower() for i in row["synonyms"])
 
-        return names_to_synonyms, all_synonyms
+        return names_to_synonyms
 
     def synonym_columns(self, csv_dict):
         result = set()
         for k, v in csv_dict.iteritems():
-            if "symptom" in k.lower():
-                result.add(self.case_it(v))
+            if "synonym" in k.lower() and v and len(v.strip()):
+                result.add(v.strip())
         return result
 
-    def new_list(self, existing_json):
+    def new_list(self):
+        names_to_synonyms = {}
         with open(self.to_load_csv, 'rb') as csvfile:
             rows = csv.DictReader(csvfile)
-            names_to_synonyms, all_synonyms = self.existing_terms_and_synonyms(existing_json)
 
             for row in rows:
-                name = self.case_it(row["Name"])
-                if name in all_synonyms:
-                    print "we already have {} as a synonym, skipping".format(name)
-                else:
-                    new_synonyms = self.synonym_columns(row)
-                    if name in names_to_synonyms:
-                        names_to_synonyms[name] = names_to_synonyms[name].union(new_synonyms)
-                    else:
-                        print "adding {}".format(name)
-                        names_to_synonyms[name] = new_synonyms
+                synonymn_columns = self.synonym_columns(row)
+                synonymn_columns = set(i.lower() for i in synonymn_columns)
+                names_to_synonyms[row["Name"].lower()] = synonymn_columns
 
-        new_list = []
-        alphabetical = sorted(names_to_synonyms.iterkeys())
+        return names_to_synonyms
 
-        for name in alphabetical:
-            new_dict = dict(name=self.case_it(name))
-            synonyms = names_to_synonyms[name]
-            new_dict["synonyms"] = sorted(self.case_it(i) for i in synonyms)
-            new_list.append(new_dict)
-
-        return new_list
-
-    def update_list(self):
+    def old_list(self):
         with open(self.lookuplist_file, 'r') as data_file:
-            existing_json = json.load(data_file)
-            existing_json[self.lookuplist_type] = self.new_list(existing_json[self.lookuplist_type])
-        with open(self.lookuplist_file, 'w') as data_file:
-            json.dump(existing_json, data_file, sort_keys=True, indent=2, separators=(',', ': '))
+            existing_json = json.load(data_file).get(self.lookuplist_type, [])
+            return self.names_to_synonyms(existing_json)
+
+    def get_synonyms(self, names_to_synonyms_dict):
+        result = set()
+
+        for synonyms in names_to_synonyms_dict.itervalues():
+            result = result.union(synonyms)
+        return result
+
+    def diff_lists(self):
+        new_dict = self.new_list()
+        old_dict = self.old_list()
+        new_names = set(new_dict.keys())
+        old_names = set(old_dict.keys())
+        new_synonyms = self.get_synonyms(new_dict)
+        old_synonyms = self.get_synonyms(old_dict)
+
+        existing_names_as_new_synonyms = new_synonyms.intersection(old_names)
+
+        new_names_as_old_synonyms = old_synonyms.intersection(new_names)
+
+        print "existing names as new synonyms"
+        pprint.pprint(existing_names_as_new_synonyms)
+
+        print "new names as existing synonyms"
+        pprint.pprint(new_names_as_old_synonyms)
+
+        names_diff = new_names.difference(old_names)
+        names_diff = names_diff.difference(new_names_as_old_synonyms)
+
+        print "new names"
+        pprint.pprint(names_diff)
+        print "========"
+        # all_new_synonyms = [i]
+        # synonyms_diff = new_synonyms.diff(old_synonyms)
+        print "synonyms diff"
+        common_names = new_names.intersection(old_names)
+
+        names_to_add = {i: v for i, v in new_dict.iteritems() if i in names_diff}
+        synonyms_to_add = {}
+
+        for common_name in common_names:
+            old_row_synonyms = old_dict[common_name]
+            new_row_synonyms = new_dict[common_name]
+            synonyms_diff = new_row_synonyms.difference(old_row_synonyms)
+            if synonyms_diff:
+                print "new synonyms for {0}".format(common_name)
+                pprint.pprint(synonyms_diff)
+                print "========"
+                synonyms_to_add[common_name] = synonyms_diff
+
+        return names_to_add, synonyms_to_add
+
+    def case_synonyms(self, synonyms):
+        cased_synonyms = []
+
+        for synonym in synonyms:
+            if synonym.islower():
+                cased_synonyms.append(synonym.title())
+            else:
+                cased_synonyms.append(synonym)
+
+        return cased_synonyms
+
+    def update_names_for_case_sensitivity(self, names_to_add):
+        cased = {}
+        result = []
+        with open(self.to_load_csv, 'rb') as csvfile:
+            rows = csv.DictReader(csvfile)
+            for row in rows:
+                name = row["Name"]
+                if name.lower() in names_to_add:
+                    if name.islower():
+                        name = name.title()
+
+                    synonyms = self.synonym_columns(row)
+                    cased[name] = self.case_synonyms(synonyms)
+
+        for name, synonyms in cased.iteritems():
+            if not synonyms:
+                synonyms = []
+            result.append(dict(name=name, synonyms=sorted(synonyms)))
+
+        return result
+
+    def update_synonyms_for_case_sensitivity(self, synonyms_to_add, old_json):
+        for d in old_json:
+            if d["name"].lower() in synonyms_to_add:
+                new_synonyms = synonyms_to_add[d["name"].lower()]
+                new_synonyms = self.case_synonyms(new_synonyms)
+                synonyms = new_synonyms + d["synonyms"]
+                d["synonyms"] = sorted(synonyms)
+
+        return old_json
 
     def sync(self):
-        self.update_list()
+        names_to_add, synonyms_to_add = self.diff_lists()
+
+        with open(self.lookuplist_file, 'r') as data_file:
+            all_existing_json = json.load(data_file)
+            existing_json = all_existing_json.get(self.lookuplist_type, [])
+            print "existing json length %s" % len(existing_json)
+            new_json = self.update_synonyms_for_case_sensitivity(synonyms_to_add, existing_json)
+            new_names = self.update_names_for_case_sensitivity(names_to_add)
+            new_json = new_json + new_names
+            print "new json length %s" % len(new_json)
+            all_existing_json[self.lookuplist_type] = new_json
+
+            to_write_file = self.lookuplist_file
+
+            if not to_write_file.endswith(".new"):
+                to_write_file = "{}.new".format(to_write_file)
+
+            with open(to_write_file, 'w+') as data_file:
+                json.dump(all_existing_json, data_file, sort_keys=True, indent=2, separators=(',', ': '))
 
 
 def run(lookuplist_type, to_load_csv):
-    loader = Loader(lookuplist_type, to_load_csv)
-    loader.sync()
+    differ = Differ(lookuplist_type, to_load_csv)
+    differ.sync()
 
 if __name__ == "__main__":
-    run(*sys.argv[:2])
+    run(*sys.argv[1:3])
