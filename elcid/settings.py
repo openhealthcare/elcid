5b099f46458a445011d776442ff683ef3d452aa7
fredkingham <fredkingham@gmail.com>
2015-11-30 11:57:00 +0000

    use django cached template loader

--- elcid/settings.py
+++ elcid/settings.py
@@ -105,9 +105,10 @@ SECRET_KEY = 'hq6wg27$1pnjvuesa-1%-wiqrpnms_kx+w4g&&o^wr$5@stjbu'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+    ('django.template.loaders.cached.Loader', (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    )),
 )
 
 MIDDLEWARE_CLASSES = (
