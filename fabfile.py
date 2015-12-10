816cec013fbe3359a51dfe481ef112200578b108
fredkingham <fredkingham@gmail.com>
2015-12-10 13:10:07 +0000

    add prompt for the fabfile so it won't necessarily proceed unless you have committed changes

--- fabfile.py
+++ fabfile.py
@@ -1,30 +1,33 @@
-from fabric.api import env, task, sudo, cd, run, local, prefix, lcd, get, execute, settings
-from fabric.context_managers import quiet
-from fabric.contrib.project import rsync_project
-from fabric.contrib.files import sed
-from datetime import datetime
-import os
-from os.path import basename, dirname
+from fabric.api import env, task, run, local, prefix
+from fabric.operations import prompt
 
 
-env.key_filename = "../../ec2.pem"
 env.hosts = ["ec2-52-16-175-249.eu-west-1.compute.amazonaws.com"]
 env.user = "ubuntu"
 virtual_env_name = "elcid-rfh"
 
 
 @task
-def deploy():
+def deploy(key_file_name="../ec2.pem"):
+    env.key_filename = key_file_name
     changes = local("git status --porcelain", capture=True)
     if len(changes):
-        print "you have uncommited changes {}".format(changes)
-    else:
-        git_branch_name = local('git rev-parse --abbrev-ref HEAD', capture=True)
-        with prefix(". /usr/share/virtualenvwrapper/virtualenvwrapper.sh"):
-            with prefix("workon {}".format(virtual_env_name)):
-                run("git pull origin {}".format(git_branch_name))
-                run("pip install -r requirements.txt")
-                run("python manage.py migrate")
-                run("python manage.py collectstatic --noinput")
-                run("pkill supervisord; pkill gunicorn")
-                run("supervisord -c etc/production.conf")
+        print " {}".format(changes)
+        proceed = prompt(
+            "you have uncommited changes, do you want to proceed",
+            default=False,
+            validate=bool
+        )
+
+        if not proceed:
+            return
+
+    git_branch_name = local('git rev-parse --abbrev-ref HEAD', capture=True)
+    with prefix(". /usr/share/virtualenvwrapper/virtualenvwrapper.sh"):
+        with prefix("workon {}".format(virtual_env_name)):
+            run("git pull origin {}".format(git_branch_name))
+            run("pip install -r requirements.txt")
+            run("python manage.py migrate")
+            run("python manage.py collectstatic --noinput")
+            run("pkill supervisord; pkill gunicorn")
+            run("supervisord -c etc/production.conf")
