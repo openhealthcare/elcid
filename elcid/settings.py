7b2ea88aaf58bda179db9f0ffd82e01f14517bfe
fredkingham <fredkingham@gmail.com>
2015-12-11 09:56:47 +0000

    bumping version number

--- elcid/settings.py
+++ elcid/settings.py
@@ -198,6 +198,11 @@ LOGGING = {
             'level': 'INFO',
             'propagate': True,
         },
+        'django.db.backends':{
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': True,
+        }
     }
 }
 
@@ -244,7 +249,7 @@ else:
     EMAIL_HOST = 'localhost'
 
 
-VERSION_NUMBER = '0.5.1'
+VERSION_NUMBER = '0.5.2'
 #TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 #TEST_RUNNER = 'django_test_coverage.runner.CoverageTestSuiteRunner'
 
