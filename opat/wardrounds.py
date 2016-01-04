7e74cb326c10224dabeb9df57460e32192028c76
fredkingham <fredkingham@gmail.com>
2016-01-04 15:49:20 +0000

    changes for the new wardround implementation

--- opat/wardrounds.py
+++ opat/wardrounds.py
@@ -1,13 +1,13 @@
-from wardround import WardRound
+from wardround.wardrounds import WardRound
 from datetime import datetime
 from opat import models
 from opal.models import Episode
+
 
 class OPATReviewList(WardRound):
     name = 'OPAT Review'
     description = 'Final review of OPAT patients post end-of-treatment'
 
-    @staticmethod
     def episodes():
         review_ready = models.OPATMeta.objects.filter(review_date__lte=datetime.date.today())
         in_round = set()
@@ -23,6 +23,5 @@ class OPATCurrentList(WardRound):
     name        = 'OPAT Current'
     description = 'All patients on the OPAT current list'
 
-    @staticmethod
     def episodes():
         return Episode.objects.filter(active=True, tagging__team__name='opat_current')
