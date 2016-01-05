8f7ab21ada60a6d0ccc2a117a93d4419c102b6e9
fredkingham <fredkingham@gmail.com>
2016-01-05 16:12:04 +0000

    move mine into elcid as we need the schema from elcid from the time being, inline forms we can actually have just set

--- elcid/patient_lists.py
+++ elcid/patient_lists.py
@@ -1,5 +1,6 @@
-from opal.core.patient_lists import TaggedPatientList
+from opal.core.patient_lists import TaggedPatientList, PatientList
 from elcid import models
+from opal.models import Episode
 
 
 class InfectionControl(TaggedPatientList):
@@ -10,3 +11,18 @@ class InfectionControl(TaggedPatientList):
         models.Location,
         models.MicrobiologyTest,
     ]
+
+
+class Mine(PatientList):
+    """
+    if the user has tagged episodes as their's this will give them the appropriate
+    episode queryset
+    """
+    @classmethod
+    def get(klass, **kwargs):
+        tag = kwargs.get("tag", None)
+        if tag and "mine" == tag.lower():
+            return klass
+
+    def get_queryset(self):
+        return Episode.objects.filter(tagging__user=self.request.user)
