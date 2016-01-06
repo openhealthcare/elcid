2fb16f4880e0e41cce46318d74a92fd7d995e866
fredkingham <fredkingham@gmail.com>
2016-01-06 16:59:43 +0000

    use traditional django logging add post request logging to the nginx conf

--- elcid/middleware.py
+++ elcid/middleware.py
@@ -8,24 +8,26 @@ logger = logging.getLogger('elcid.requestLogger')
 
 
 class LoggingMiddleware(object):
-    pass
+    def process_request(self, request):
+        self.start_time = time.time()
 
-    # def process_request(self, request):
-    #     self.start_time = time.time()
-    #
-    # def process_response(self, request, response):
-    #     username = "-"
-    #     extra_log = ""
-    #     if hasattr(request, 'user'):
-    #         username = getattr(request.user, 'username', '-')
-    #     req_time = time.time() - self.start_time
-    #
-    #     if settings.DEBUG:
-    #         sql_time = sum(float(q['time']) for q in connection.queries) * 1000
-    #         extra_log += " (%s SQL queries, %s ms)" % (len(connection.queries), sql_time)
-    #
-    #     logger.info("%s %s %s %s %s (%.02f seconds)%s" % (
-    #         datetime.now(), username, request.method, request.get_full_path(),
-    #         response.status_code, req_time, extra_log)
-    #     )
-    #     return response
+    def process_response(self, request, response):
+        try:
+            username = "-"
+            extra_log = ""
+            if hasattr(request, 'user'):
+                username = getattr(request.user, 'username', '-')
+            req_time = time.time() - self.start_time
+
+            if settings.DEBUG:
+                sql_time = sum(float(q['time']) for q in connection.queries) * 1000
+                extra_log += " (%s SQL queries, %s ms)" % (len(connection.queries), sql_time)
+
+            logger.info("%s %s %s %s %s (%.02f seconds)%s" % (
+                datetime.now(), username, request.method, request.get_full_path(),
+                response.status_code, req_time, extra_log)
+            )
+        except Exception, e:
+            logging.error("LoggingMiddleware Error: %s" % e)
+            
+        return response
