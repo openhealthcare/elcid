e9a3fb336ff9d605afb64c0c260570663f279e9c
fredkingham <fredkingham@gmail.com>
2016-02-09 15:17:17 +0000

    added checkout environment script

--- fabfile.py
+++ fabfile.py
@@ -5,21 +5,21 @@ import os
 env.hosts = ["elcid-uch-test.openhealthcare.org.uk"]
 env.user = "ubuntu"
 project_name = "elcid"
+fabfile_dir = os.path.realpath(os.path.dirname(__file__))
 
 def get_requirements():
     """
     looks for a requirements file in the same directory as the
     fabfile. Parses it,
     """
-    fabfile_dir = os.path.realpath(os.path.dirname(__file__))
 
     with lcd(fabfile_dir):
         requirements = local("less requirements.txt", capture=True).split("\n")
+
         package_to_version = {}
 
         for requirement in requirements:
-            s = requirement.split("===")
-            package_to_version[s[0]] = s[1]
+            package_to_version.update(pass_github_urls(requirement))
 
     return package_to_version
 
@@ -29,24 +29,20 @@ def pass_github_urls(some_url):
     -e git+https://github.com/openhealthcare/opal-referral.git@v0.1.2#egg=opal_referral
     returns opal-referral
     """
-    if "github" in some_url:
+
+    if "github" in some_url and "opal" in some_url:
         package_name = some_url.split("@")[0].split("/")[-1]
-        version = some_url.split("@")[-1].split("/")[-1]
-
-
+        version = some_url.split("@")[-1].split("#")[0]
+        return {package_name: version}
 
 @task
-def checkout_current_environment():
-    """
-    looks for a requirements file in the same directory as the
-    fabfile. Parses it,
-    """
-    fabfile_dir = os.path.realpath(os.path.dirname(__file__))
-
+def checkout_project(package_name_version):
     with lcd(fabfile_dir):
-        requirements = local("less requirements.txt", capture=True).split("\n")
-        package_to_version = {}
+        with lcd(".."):
+            existing_packages = local("ls").split("\n")
 
-        for requirement in requirements:
-            s = requirement.split("===")
-            package_to_version[s[0]] = s[1]
+            for package_name, version in package_name_version.iteritems():
+                if package_name in existing_packages:
+                    print "we are going to checkout {}".format(package_name)
+                else:
+                    print "we are going to clone {}".format(package_name)
