5aa4242476a5338147279d3933d9ab69d7c797b7
fredkingham <fredkingham@gmail.com>
2016-02-10 13:41:08 +0000

    correct virtualenvwrapper location

--- fabfile.py
+++ fabfile.py
@@ -149,7 +149,7 @@ def test_deploy(branch):
     env_name = "elcid{}".format(env_name)
 
     # assumes we've pip installed virtualenvwrapper
-    with prefix("/usr/local/virtualenvwrapper.sh"):
+    with prefix("/usr/local/bin/virtualenvwrapper.sh"):
         local('mkproject '.format(env_name))
         with prefix("workon ".format(env_name)):
             local("git clone -b {0} {1}".format(branch, github_url))
