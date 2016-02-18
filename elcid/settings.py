8244c3c90c13ae15ac752da2ad471cdfab226e3e
fredkingham <fredkingham@gmail.com>
2016-02-18 11:39:03 +0000

    changes for the new tagging release also sets async to false for heroku testing, that needs to be backed out

--- elcid/settings.py
+++ elcid/settings.py
@@ -286,4 +286,4 @@ except:
 # MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + ('debug_toolbar.middleware.DebugToolbarMiddleware',)
 # INSTALLED_APPS = INSTALLED_APPS + ('debug_toolbar',)
 # #INTERNAL_IPS = ('127.0.0.1',)
-EXTRACT_ASYNC = True
+EXTRACT_ASYNC = False
