388196f1e69564e0b52b19e471af0c8c6eb4b49c
David Miller <david@deadpansincerity.com>
2016-02-24 14:12:28 +0000

    Lists are at /list/ now

--- elcid/assets/js/elcid/routes.js
+++ elcid/assets/js/elcid/routes.js
@@ -2,6 +2,5 @@ var app = angular.module('opal');
 app.config(
     ['$routeProvider',
      function($routeProvider){
-	     $routeProvider.when('/',  {redirectTo: '/list'})
+	     $routeProvider.when('/',  {redirectTo: '/list/'})
      }]);
-
