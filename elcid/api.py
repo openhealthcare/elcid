df80a6e2a4c4f706fcbbee0709526f94b4e00664
fredkingham <fredkingham@gmail.com>
2016-03-21 16:20:05 +0000

    added api and associated unit tests

---
+++ elcid/api.py
@@ -0,0 +1,45 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseForbidden
+from django.conf import settings
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.mixins import UpdateModelMixin, RetrieveModelMixin
+from opal.core.api import OPALRouter
+from opal.models import Patient
+
+
+class GlossEndpointApi(RetrieveModelMixin, UpdateModelMixin, viewsets.ViewSet):
+    base_name = 'glossapi'
+
+    def login(self, request):
+        user = authenticate(
+            username=settings.GLOSS_USERNAME, password=settings.GLOSS_PASSWORD
+        )
+        if user is not None:
+            login(request, user)
+        else:
+            raise HttpResponseForbidden()
+        return user
+
+    def update(self, request, pk=None):
+        hospital_number = pk
+        request_data = request.data
+        update_dict = request_data["data"]
+
+        patient_query = Patient.objects.filter(
+            demographics__hospital_number=hospital_number
+        )
+
+        if not patient_query.exists():
+            patient = Patient()
+        else:
+            patient = patient_query.get()
+
+        user = self.login(request)
+
+        patient.bulk_update(update_dict, user)
+        return Response("ok")
+
+
+router = OPALRouter()
+router.register('glossapi', GlossEndpointApi)
--- elcid/urls.py
+++ elcid/urls.py
@@ -1,12 +1,13 @@
 """
 Root elCID urlconf
 """
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 from django.contrib import admin
 
 admin.autodiscover()
 
 from opal.urls import urlpatterns as opatterns
+from elcid import api
 from microhaem.urls import urlpatterns as mpatterns
 
 from elcid import views
@@ -19,6 +20,7 @@ urlpatterns = patterns(
     url(r'^test/500$', views.Error500View.as_view(), name='test-500'),
     url(r'^templates/elcid/modals/(?P<name>[a-z_]+.html)$', views.ElcidTemplateView.as_view()),
     url(r'stories/$', views.TemplateView.as_view(template_name='stories.html')),
+    url(r'glossapi/v0.1/', include(api.router.urls)),
 )
 
 urlpatterns += opatterns
