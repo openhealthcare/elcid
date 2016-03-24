8d9f7ec2412161ca3236e21a002674cba2025725
David Miller <david@deadpansincerity.com>
2016-03-24 15:02:23 +0000

    More flow service tests:

--- elcid/assets/js/elcidtest/flow.service.test.js
+++ elcid/assets/js/elcidtest/flow.service.test.js
@@ -3,10 +3,22 @@ describe('elCIDFlow', function() {
     var $routeParams;
     var Flow;
 
-    var inpatient_default = {
+    var walkin_enter = {
+        'controller': 'WalkinHospitalNumberCtrl',
+        'template'  : '/templates/modals/hospital_number.html/'
+    };
+    var walkin_exit =  {
+        'controller': 'WalkinDischargeCtrl',
+        'template'  : '/templates/modals/discharge_walkin_episode.html/'
+    };
+    var inpatient_default_enter = {
         'controller': 'HospitalNumberCtrl',
         'template'  : '/templates/modals/hospital_number.html/'
-    }
+    };
+    var inpatient_default_exit =  {
+        'controller': 'ElcidDischargeEpisodeCtrl',
+        'template'  : '/templates/modals/discharge_episode.html/'
+    };
 
     beforeEach(function(){
         module('opal.services');
@@ -17,11 +29,30 @@ describe('elCIDFlow', function() {
 
     });
 
+    describe('Walkin', function() {
+
+        beforeEach(function(){
+            $routeParams.slug = 'walkin-walkin_doctor';
+        });
+
+        it('should enter should fetch the flow', function() {
+            expect(Flow.enter()).toEqual(walkin_enter);
+        });
+
+        it('should fetch the exit flow', function() {
+            expect(Flow.exit({category: 'Walkin'})).toEqual(walkin_exit);
+        });
+
+    });
+
     describe('Inpatients', function() {
-        describe('enter', function() {
-            it('should fetch the default flow', function() {
-                expect(Flow.enter()).toEqual(inpatient_default);
-            });
+
+        it('enter should fetch the default flow', function() {
+            expect(Flow.enter()).toEqual(inpatient_default_enter);
+        });
+
+        it('should fetch hte default exit flow', function() {
+            expect(Flow.exit({category: 'Inpatient'})).toEqual(inpatient_default_exit);
         });
     });
 
