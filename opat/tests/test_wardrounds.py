39b02b188497c8516f04b3de0f0d46d27956c057
fredkingham <fredkingham@gmail.com>
2016-05-11 12:00:53 +0100

    remove duplicate unit tests

--- opat/tests/test_wardrounds.py
+++ opat/tests/test_wardrounds.py
@@ -1,13 +1,30 @@
 """
 Unittests for elcid.opat.wardrounds
 """
+import datetime
+from mock import MagicMock
 from opal.core.test import OpalTestCase
-
+from opal.models import Tagging
 from opat import wardrounds
+from opat import models
 
-class OpatCurrentTestCase(OpalTestCase):
+
+class OPATCurrentListTestCase(OpalTestCase):
     def test_episodes(self):
         patient, episode = self.new_patient_and_episode_please()
+        episode.active = True
+        episode.save()
         episode.set_tag_names(['opat', 'opat_current'], self.user)
-        self.assertEqual([episode],
-                         list(wardrounds.OPATCurrentList(None).episodes()))
+        mock_request = MagicMock()
+        wardround = wardrounds.OPATCurrentList(mock_request)
+        self.assertEqual(episode, wardround.episodes().get())
+
+
+class OPATReviewListTestCase(OpalTestCase):
+    def test_episodes(self):
+        yesterday = datetime.date.today() - datetime.timedelta(1)
+        patient, episode = self.new_patient_and_episode_please()
+        models.OPATMeta.objects.create(episode=episode, review_date=yesterday)
+        mock_request = MagicMock()
+        wardround = wardrounds.OPATReviewList(mock_request)
+        self.assertEqual(episode, wardround.episodes().get())
