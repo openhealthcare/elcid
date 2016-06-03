4ad9dbfae63ee749118c379c1a68bc374ad35a91
fredkingham <fredkingham@gmail.com>
2016-06-03 15:25:04 +0100

    use the visible feature of discoverable

--- elcid/detail.py
+++ elcid/detail.py
@@ -2,8 +2,13 @@ from opal.core import detail
 from django.conf import settings
 
 
-if settings.GLOSS_ENABLED:
-    class Result(detail.PatientDetailView):
-        display_name = "Test Results"
-        order = 5
-        template = "detail/result.html"
+class Result(detail.PatientDetailView):
+    display_name = "Test Results"
+    order = 5
+    template = "detail/result.html"
+
+    @classmethod
+    def visible_to(klass, user):
+        if user.is_superuser:
+            return True
+        return settings.GLOSS_ENABLED
