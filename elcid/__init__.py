6c52a9392f2e6b6fac2a8053c65108c18bde998d
fredkingham <fredkingham@gmail.com>
2016-06-24 13:05:14 +0100

    move the remove from mine into a button

--- elcid/__init__.py
+++ elcid/__init__.py
@@ -21,7 +21,8 @@ class Application(application.OpalApplication):
     ]
 
     actions = [
-        'actions/presenting_complaint.html'
+        'actions/presenting_complaint.html',
+        'actions/mine.html'
     ]
 
     patient_view_forms = {
