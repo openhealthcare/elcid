09da0ca6b9d4745ed6794ba0c42bbb66ec33e9d5
fredkingham <fredkingham@gmail.com>
2016-07-22 11:49:43 +0100

    remove event ordering by haem as this is now done in the patient loader, add unit tests for clinical advice ordering

--- elcid/assets/js/elcidtest/haem_view.test.js
+++ elcid/assets/js/elcidtest/haem_view.test.js
@@ -3,6 +3,20 @@ describe('HaemView', function() {
 
   var controller;
   var $rootScope;
+  var fakeEpisodes = [
+    {
+      id: 1,
+      when: moment(new Date(2015, 1, 1)),
+      created: moment(new Date(2014, 1, 1)),
+    },
+    {
+      id: 2
+    },
+    {
+      id: 3,
+      created: moment(new Date(2014, 1, 1)),
+    },
+  ];
 
   beforeEach(function(){
       var $controller;
@@ -18,22 +32,14 @@ describe('HaemView', function() {
   });
 
   describe("it should order an event based on an episode date hierachy", function(){
-      it("should use discharge_date as the higest priority", function(){
-          var fakeEpisode = {
-              discharge_date: moment(new Date(2015, 1, 1)),
-              date_of_episode: moment(new Date(2014, 1, 1)),
-              date_of_admission: moment(new Date(2013, 1, 1)),
-          };
-          var ordering = controller.getEpisodeOrdering(fakeEpisode);
-          expect(ordering).toBe(-1422748800);
+    it("should order clinical advice when then created by then undefined", function(){
+      var ordered_events = _.sortBy(fakeEpisodes, function(x){
+          return controller.clinicalAdviceOrdering(x);
       });
 
-      it("should use date_of_admission if no other date is available", function(){
-        var fakeEpisode = {
-            date_of_admission: moment(new Date(2015, 1, 1)),
-        };
-        var ordering = controller.getEpisodeOrdering(fakeEpisode);
-        expect(ordering).toBe(-1422748800);
-      });
+      expect(ordered_events[0].id).toBe(1);
+      expect(ordered_events[1].id).toBe(3);
+      expect(ordered_events[2].id).toBe(2);
+    });
   });
 });
