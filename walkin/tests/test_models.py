b0a27693995f73682ffcd0b4a7c5db8c2e744bd7
fredkingham <fredkingham@gmail.com>
2016-07-26 20:21:21 +0100

    removing test that no longer applies

--- walkin/tests/test_models.py
+++ walkin/tests/test_models.py
@@ -3,12 +3,6 @@ from walkin import models, patient_lists
 
 
 class CustomSymptomsModelTestCase(OpalTestCase):
-    def test_get_modal_detail(self):
-        modal_template = models.Symptom.get_modal_template(
-            patient_list=patient_lists.WalkinDoctor()
-        )
-        self.assertEqual(modal_template, "modals/walkin/symptom_modal.html")
-
     def test_get_record_display(self):
         record_template = models.Symptom.get_display_template(
             patient_list=patient_lists.WalkinDoctor()
