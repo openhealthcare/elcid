35c0c1281135af53f4db71ce5f42383eefddb3b5
fredkingham <fredkingham@gmail.com>
2016-11-14 14:18:20 +0000

    fixes the clinical advice issues

--- elcid/assets/js/elcidtest/clinical_advice.controller.test.js
+++ elcid/assets/js/elcidtest/clinical_advice.controller.test.js
@@ -1,52 +1,77 @@
 describe('ClinicalAdviceFormTest', function() {
-    "use strict";
+  "use strict";
 
-    var $scope, $httpBackend, $rootScope, $controller;
-    var Episode;
-    var mkcontroller;
+  var $scope, $httpBackend, $rootScope, $controller;
+  var Episode;
+  var mkcontroller;
 
-    var episodedata = {
-        demographics: [ { patient_id: 123 } ]
+  var episodedata = {
+    demographics: [ { patient_id: 123 } ]
+  }
+  var recorddata = {
+    'microbiology_input': {
+      'name': 'microbiology_input',
+      'fields': []
     }
-    var recorddata = {
-            'microbiology_input': {
-                'name': 'microbiology_input',
-                'fields': []
-            }
+  };
+
+  var mockReferenceData = {
+    toLookuplists: function(){
+      return {some_list: []}
+    }
+  };
+
+  var mockReferenceDataPromise = {
+    then: function(someFun){
+      return someFun(mockReferenceData);
+    }
+  }
+
+  beforeEach(function(){
+
+    module('opal.controllers');
+
+    inject(function($injector){
+      $httpBackend = $injector.get('$httpBackend');
+      $rootScope   = $injector.get('$rootScope');
+      $scope       = $rootScope.$new();
+      $controller  = $injector.get('$controller');
+      Episode      = $injector.get('Episode');
+    });
+
+    $scope.episode = new Episode(episodedata);
+
+    mkcontroller = function(){
+      return $controller('ClinicalAdviceForm', {
+        $rootScope: $rootScope,
+        $scope: $scope,
+        Referencedata: mockReferenceDataPromise
+      });
     };
+    $httpBackend.expectGET('/api/v0.1/userprofile/').respond({});
+    $httpBackend.expectGET('/api/v0.1/record/').respond(recorddata);
+  });
 
-    beforeEach(function(){
+  afterEach(function(){
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
 
-        module('opal.controllers');
-
-        inject(function($injector){
-            $httpBackend = $injector.get('$httpBackend');
-            $rootScope   = $injector.get('$rootScope');
-            $scope       = $rootScope.$new();
-            $controller  = $injector.get('$controller');
-            Episode      = $injector.get('Episode');
-        });
-
-        $scope.episode = new Episode(episodedata);
-
-        mkcontroller = function(){
-            var ret = $controller('ClinicalAdviceForm', {
-                $rootScope: $rootScope,
-                $scope: $scope,
-            });
-        };
-        $httpBackend.expectGET('/api/v0.1/userprofile/').respond({});
-        $httpBackend.expectGET('/api/v0.1/record/').respond(recorddata);
+  describe('initialization', function() {
+    it('should put look up lists on the scope', function(){
+      var ctrl = mkcontroller();
+      $httpBackend.flush();
+      expect(ctrl.some_list).toEqual([]);
     });
 
-    describe('initialization', function() {
+    it('should populate editing with a microbiology_input record', function(){
+      var ctrl = mkcontroller();
+      $rootScope.$apply();
+      $httpBackend.flush();
 
-        it('should setup', function() {
-            mkcontroller();
-            $rootScope.$apply();
-            $httpBackend.flush();
-        });
-
+      expect(!!ctrl.editing).toBe(true);
+      expect(_.contains(_.keys(ctrl.editing), "reason_for_interaction")).toBe(true);
+      expect(_.contains(_.keys(ctrl.editing), "discussed_with")).toBe(true);
     });
-
+  });
 });
