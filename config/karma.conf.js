adbc103949cee80a03379bee3274a6fbd303b185
fredkingham <fredkingham@gmail.com>
2016-11-23 18:41:22 +0000

    update to import the default karma config

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,70 +1,42 @@
-module.exports =  function(config){
-    var browsers, basePath, coverageReporter;
-    var additionalDependencies;
+module.exports = function(config){
+  var opalPath;
+  if(process.env.TRAVIS){
+    python_version = process.env.TRAVIS_PYTHON_VERSION;
+    opalPath = '/home/travis/virtualenv/python' + python_version + '/src/opal';
+  }
+  else{
+    opalPath = '../../opal';
+  }
+  var karmaDefaults = require(opalPath + '/config/karma_defaults.js');
+  var karmaDir = __dirname;
+  var coverageFiles = [
+    __dirname + '/../elcid/assets/js/elcid/*',
+    __dirname + '/../elcid/assets/js/elcid/controllers/*',
+    __dirname + '/../elcid/assets/js/elcid/services/*',
+    __dirname + '/../elcid/assets/js/elcid/services/records/*',
+    __dirname + '/../opat/static/js/opat/controllers/*',
+    __dirname + '/../walkin/static/js/walkin/controllers/*'
+  ];
+  var includedFiles = [
+    'opal/app.js',
+    // Our application
 
-    if(process.env.TRAVIS){
-        browsers = ["Firefox"];
-        basePath = '/home/travis/virtualenv/python2.7/src/opal/opal/static/js';
-        coverageReporter = {
-            type: 'lcovonly', // lcov or lcovonly are required for generating lcov.info files
-            dir: __dirname + '/../coverage/',
-        };
-        additionalDependencies = require('/home/travis/virtualenv/python2.7/src/opal/config/karma_dependencies.js');
-    }
-    else{
-        browsers = ['PhantomJS'];
-        basePath = '../../opal/opal/static/js';
-        coverageReporter = {
-            type : 'html',
-            dir : __dirname + '/../htmlcov/js/'
-        };
-        additionalDependencies = require('../../opal/config/karma_dependencies.js');
-    }
-
-    var preprocessors = {};
-    preprocessors[__dirname + '/../elcid/assets/js/elcid/*'] = 'coverage';
-    preprocessors[__dirname + '/../elcid/assets/js/elcid/controllers/*'] = 'coverage';
-    preprocessors[__dirname + '/../elcid/assets/js/elcid/services/*'] = 'coverage';
-    preprocessors[__dirname + '/../elcid/assets/js/elcid/services/records/*'] = 'coverage';
-    preprocessors[__dirname + '/../opat/static/js/opat/controllers/*'] = 'coverage';
-    preprocessors[__dirname + '/../walkin/static/js/walkin/controllers/*'] = 'coverage';
-
-    config.set({
-        frameworks: ['jasmine'],
-        browsers: browsers,
-        basePath:  basePath,
-
-        files: additionalDependencies().concat([
-          'opal/app.js',
-          // Our application
-
-          __dirname + '/../elcid/assets/js/elcid/*.js',
-          __dirname + '/../elcid/assets/js/elcid/controllers/*.js',
-          __dirname + '/../elcid/assets/js/elcid/services/*.js',
-          __dirname + '/../elcid/assets/js/elcid/services/records/*.js',
-          __dirname + '/../opat/static/js/opat/controllers/*.js',
-          __dirname + '/../research/static/js/research/controllers/*.js',
-          __dirname + '/../walkin/static/js/walkin/controllers/*.js',
+    __dirname + '/../elcid/assets/js/elcid/*.js',
+    __dirname + '/../elcid/assets/js/elcid/controllers/*.js',
+    __dirname + '/../elcid/assets/js/elcid/services/*.js',
+    __dirname + '/../elcid/assets/js/elcid/services/records/*.js',
+    __dirname + '/../opat/static/js/opat/controllers/*.js',
+    __dirname + '/../research/static/js/research/controllers/*.js',
+    __dirname + '/../walkin/static/js/walkin/controllers/*.js',
 
 
-          // The tests
-          __dirname + '/../elcid/assets/js/elcidtest/*.js',
-          __dirname + '/../opat/static/js/test/*.js',
-          __dirname + '/../research/static/js/test/*.js',
-          __dirname + '/../walkin/static/js/walkintest/*.js',
-        ]),
+    // The tests
+    __dirname + '/../elcid/assets/js/elcidtest/*.js',
+    __dirname + '/../opat/static/js/test/*.js',
+    __dirname + '/../research/static/js/test/*.js',
+    __dirname + '/../walkin/static/js/walkintest/*.js'
+  ];
 
-        preprocessors: preprocessors,
-
-        reporters: ['progress', 'coverage'],
-        autoWatch: true,
-
-        coverageReporter: coverageReporter,
-
-        // Stolen from http://oligofren.wordpress.com/2014/05/27/running-karma-tests-on-browserstack/
-        browserDisconnectTimeout : 10000, // default 2000
-        browserDisconnectTolerance : 1, // default 0
-        browserNoActivityTimeout : 4*60*1000, //default 10000
-        captureTimeout : 4*60*1000, //default 60000
-    });
-}
+  var defaultConfig = karmaDefaults(karmaDir, coverageFiles, includedFiles);
+  config.set(defaultConfig);
+};
