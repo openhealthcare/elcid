79035777c1fdc58ebbbc46515db48024d5fef634
Fred <fredkingham@users.noreply.github.com>
2016-11-24 21:57:17 +0000

    Update karma.conf.js

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -8,7 +8,7 @@ module.exports = function(config){
     opalPath = '../../opal';
   }
   var karmaDefaults = require(opalPath + '/config/karma_defaults.js');
-  var baseDir = '../' + __dirname;
+  var baseDir = __dirname + '/..';
   var coverageFiles = [
     __dirname + '/../elcid/assets/js/elcid/*',
     __dirname + '/../elcid/assets/js/elcid/controllers/*',
