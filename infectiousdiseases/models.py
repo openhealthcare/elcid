dd8355e921bbf266d974e5e77363c5cc95c3d305
fredkingham <fredkingham@gmail.com>
2017-01-30 15:59:25 +0000

    add a contact notes text field

--- infectiousdiseases/models.py
+++ infectiousdiseases/models.py
@@ -9,24 +9,26 @@ class ExternalLiasonContactDetails(EpisodeSubrecord):
 
     # required
     external_hospital_number = models.CharField(
-        max_length=256,
+        max_length=256, null=True, blank=True
     )
 
     # required
     hospital = models.CharField(
-        max_length=256
+        max_length=256, null=True, blank=True
     )
 
     hospital_contact = models.CharField(
-        max_length=256
+        max_length=256, null=True, blank=True
     )
 
     contact_telephone_number = models.CharField(
-        max_length=256
+        max_length=256, null=True, blank=True
     )
 
     # not using an email field because at the
     # moment validation won't flow through
     contact_email_address = models.CharField(
-        max_length=256
+        max_length=256, null=True, blank=True
     )
+
+    contact_notes = models.TextField(null=True, blank=True)
