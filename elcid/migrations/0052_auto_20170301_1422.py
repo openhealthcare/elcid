4b07f186f0fe7191198bf6ac98648b6fca721a55
fredkingham <fredkingham@gmail.com>
2017-03-01 13:54:01 +0000

    we've moved to concrete models

---
+++ elcid/migrations/0052_auto_20170301_1422.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('elcid', '0051_auto_20160725_1222'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='pastmedicalhistory',
+            options={},
+        ),
+        migrations.AlterField(
+            model_name='allergies',
+            name='drug_fk',
+            field=models.ForeignKey(blank=True, to='opal.Drug', null=True),
+        ),
+        migrations.AlterField(
+            model_name='allergies',
+            name='provisional',
+            field=models.BooleanField(default=False, verbose_name=b'Suspected?'),
+        ),
+        migrations.AlterField(
+            model_name='demographics',
+            name='gp_practice_code',
+            field=models.CharField(max_length=20, null=True, verbose_name=b'GP Practice Code', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='demographics',
+            name='nhs_number',
+            field=models.CharField(max_length=255, null=True, verbose_name=b'NHS Number', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='diagnosis',
+            name='provisional',
+            field=models.BooleanField(default=False, verbose_name=b'Provisional?'),
+        ),
+        migrations.AlterField(
+            model_name='microbiologytest',
+            name='anti_hbcore_igg',
+            field=models.CharField(max_length=20, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='microbiologytest',
+            name='anti_hbcore_igm',
+            field=models.CharField(max_length=20, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='microbiologytest',
+            name='anti_hbs',
+            field=models.CharField(max_length=20, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='microbiologytest',
+            name='ebna_igg',
+            field=models.CharField(max_length=20, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='microbiologytest',
+            name='hbsag',
+            field=models.CharField(max_length=20, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='microbiologytest',
+            name='hsv',
+            field=models.CharField(max_length=20, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='microbiologytest',
+            name='igg',
+            field=models.CharField(max_length=20, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='microbiologytest',
+            name='igm',
+            field=models.CharField(max_length=20, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='microbiologytest',
+            name='rpr',
+            field=models.CharField(max_length=20, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='microbiologytest',
+            name='tppa',
+            field=models.CharField(max_length=20, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='microbiologytest',
+            name='vca_igg',
+            field=models.CharField(max_length=20, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='microbiologytest',
+            name='vca_igm',
+            field=models.CharField(max_length=20, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='microbiologytest',
+            name='vzv',
+            field=models.CharField(max_length=20, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='pastmedicalhistory',
+            name='year',
+            field=models.CharField(max_length=4, blank=True),
+        ),
+    ]
