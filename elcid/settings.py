11519cc2701ef89d2129152bbc59eedca7b6ce56
fredkingham <fredkingham@gmail.com>
2018-01-11 22:33:47 +0000

    bumping the version number

--- elcid/settings.py
+++ elcid/settings.py
@@ -301,7 +301,7 @@ else:
     EMAIL_HOST = 'localhost'
 
 
-VERSION_NUMBER = '0.7.0.1'
+VERSION_NUMBER = '0.7.0.2'
 
 #TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 #TEST_RUNNER = 'django_test_coverage.runner.CoverageTestSuiteRunner'
