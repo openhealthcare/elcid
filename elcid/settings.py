f1f47f032257fb96475670111647a62b78e77f3c
fredkingham <fredkingham@gmail.com>
2018-01-22 17:55:53 +0000

    bumps the version changes the wardround dependency

--- elcid/settings.py
+++ elcid/settings.py
@@ -301,7 +301,7 @@ else:
     EMAIL_HOST = 'localhost'
 
 
-VERSION_NUMBER = '0.7.0.2'
+VERSION_NUMBER = '0.7.0.3'
 
 #TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 #TEST_RUNNER = 'django_test_coverage.runner.CoverageTestSuiteRunner'
