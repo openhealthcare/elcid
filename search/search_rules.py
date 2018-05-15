1fea379f865109ae372930f8d90aa8435dd379ec
fredkingham <fredkingham@gmail.com>
2018-05-15 21:05:05 +0100

    moves out elcid specific search rules into elcid

--- search/search_rules.py
+++ search/search_rules.py
@@ -125,34 +125,3 @@ class EpisodeQuery(SearchRule):
         search_rule_fields.EpisodeStart,
         search_rule_fields.EpisodeEnd
     )
-
-
-class ContactDetailsSearchRule(SearchRule):
-    slug = emodels.ContactDetails.get_api_name()
-    display_name = "Patient Contact Details"
-    model = emodels.ContactDetails
-
-
-class ResultQuery(SearchRule):
-    exclude = True
-    slug = emodels.Result.get_api_name()
-
-
-class MicroTestRule(SearchRule):
-    slug = emodels.MicrobiologyTest.get_api_name()
-    model = emodels.MicrobiologyTest
-
-    fields = [
-        "test",
-        "date_ordered",
-        "details",
-        "microscopy",
-        "organism",
-        "sensitive_antibiotics",
-        "resistant_antibiotics"
-    ]
-
-
-class DuplicatePatientQuery(SearchRule):
-    exclude = True
-    slug = emodels.DuplicatePatient.get_api_name()
