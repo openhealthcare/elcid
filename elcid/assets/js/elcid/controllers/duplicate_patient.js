9237d60536e0ba1340d5915b95c7d0aa384de1ff
fredkingham <fredkingham@gmail.com>
2018-05-18 13:17:00 +0100

    removes the unused duplicate patient controller

--- elcid/assets/js/elcid/controllers/duplicate_patient.js
+++
@@ -1,9 +0,0 @@
-angular.module('opal.controllers').controller(
-    'DuplicatePatientCtrl', function($rootScope, $scope, $window, ngProgressLite){
-
-      self.save = function(){
-          ngProgressLite.set(0);
-          ngProgressLite.start()
-      };
-
-});
